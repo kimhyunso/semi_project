4477247545b2d43d126f5c0e1ba82507aab65fae
kimhyunso <gustn9515@gmail.com>
2023-04-03 17:55:03 +0900

    20230403 17:55

--- Preprocessing.py
+++ Preprocessing.py
@@ -23,6 +23,7 @@ class Preprocessing:
         # target data 분리
         self.__y_target = self.__pay_df['Default']
         del self.__pay_df['Default']
+        # 특정 컬럼에 대한 결측치 처리
         self.__pay_df['Client_Occupation'].fillna('Nojob', inplace=True)
         self.__pay_df['Credit_Bureau'].fillna(self.__pay_df['Credit_Bureau'].mean(), inplace=True)
 
@@ -36,12 +37,14 @@ class Preprocessing:
         # object인 column들(카테고리)만 뽑기
         category_columns_object = self.__pay_df.select_dtypes(include='object').columns
 
+        # 결측치 0개 초과 10000개 미만의 데이터를 대상으로 랜덤하게 결측치 대체
         for column in category_columns_object:
             if self.__pay_df[column].isna().sum() > 10000 or self.__pay_df[column].isna().sum() == 0:
                 continue
             unique_columns = self.__pay_df[column].loc[self.__pay_df[column].isna()==False].unique()
             self.__pay_df[column] = self.__pay_df[column].apply(lambda x : random.choice(unique_columns) if pd.isna(x) else x)
 
+        # 나머지 범주형 데이터에 대해 one-hot encoding 적용
         df_null_sum = self.__pay_df.isna().sum()
         column = df_null_sum[df_null_sum>0].index
         self.__pay_df = pd.get_dummies(self.__pay_df, columns=column)
@@ -58,11 +61,9 @@ class Preprocessing:
         for column in numerical_columns:
             self.__pay_df[column] = self.__pay_df[column].fillna(self.__pay_df[column].mean())
 
-
     def train_split(self):
         train_x, test_x, train_y, test_y = train_test_split(test_size=0.2, random_state=42, stratify=self.__y_target)
         smote = SMOTE(random_state=42)
-
         X_train_over, y_train_over = smote.fit_sample(X_train, y_train)
 
     # data를 리턴하는 함수
